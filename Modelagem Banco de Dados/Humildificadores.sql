e0f47816316ac70cae8457f496c03b38ba4f48c3
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2020-05-01 17:15:22 -0300

    Alteração do Banco colocando temperatura e umidade de preferencia

--- Modelagem Banco de Dados/Humildificadores.sql
+++ Modelagem Banco de Dados/Humildificadores.sql
@@ -10,6 +10,8 @@ create table cliente (
 	ddn date,
 	email varchar (30),
 	senha varchar (15),
+    preferenciaTemp varchar (7),
+    preferenciaUmid varchar (3),
 	fkParque int
 )auto_increment = 1;
 
@@ -39,11 +41,11 @@ create table eventos (
 );
 
 insert into cliente values 
-(null, 'Kalil Bego', 'Rua Barreiro', 'Carapicuíba', 'São Paulo', '2020-12-20', 'Kalilmito@gmail.com', 'senha', 1),
-(null, 'Kaio Katiau', 'Rua Camarelo', 'Osasco', 'São Paulo', '2002-02-01', 'Kaiosilva@gmail.com', 'katiau', 3),
-(null, 'Eric Viezzer', 'Rua Jacinto Neves', 'Parati', 'Rio de Janeiro', '2010-10-03', 'Capivara@gmail.com', 'Capivara', 1),
-(null, 'Victor Barbosa', 'Rua Bacalhau', 'Butão', 'Goiana', '2030-09-01', 'Victor@gmail.com', 'senha', 2),
-(null, 'Gabriel Marcolino', 'Rua Nada', 'Piauzinho', 'Sergipe', '2005-03-15', 'Marcoli@gmail.com', 'senha', 2);
+(null, 'Kalil Bego', 'Rua Barreiro', 'Carapicuíba', 'São Paulo', '2020-12-20', 'Kalilmito@gmail.com', 'senha', '22.00°C', '50%', 1),
+(null, 'Kaio Katiau', 'Rua Camarelo', 'Osasco', 'São Paulo', '2002-02-01', 'Kaiosilva@gmail.com', 'katiau', '20.00°C', '20%', 3),
+(null, 'Eric Viezzer', 'Rua Jacinto Neves', 'Parati', 'Rio de Janeiro', '2010-10-03', 'Capivara@gmail.com', 'Capivara', '15.00°C', '70%', 1),
+(null, 'Victor Barbosa', 'Rua Bacalhau', 'Butão', 'Goiana', '2030-09-01', 'Victor@gmail.com', 'senha', '25.00°C', '50%', 2),
+(null, 'Gabriel Marcolino', 'Rua Nada', 'Piauzinho', 'Sergipe', '2005-03-15', 'Marcoli@gmail.com', 'senha', '30.00°C', '30%', 2);
 
 insert into parque values
 (1, 'Rua Aviões do forró', 'Muito bom', 1),
@@ -57,14 +59,14 @@ insert into sensor values
 
 insert into eventos values 
 (1, 1, '06:00', '2020-12-02', '20%', '22.00°C'),
-(1, 2, '12:00', '2020-12-03', '30%', '23.03°C'),
-(1, 3, '18:00', '2020-12-04', '25%', '23.09°C'),
-(2, 1, '06:00', '2020-05-02', '30%', '24.05°C'),
-(2, 2, '12:00', '2020-05-03', '50%', '21.02°C'),
-(2, 3, '18:00', '2020-05-04', '45%', '30.06°C'),
-(3, 1, '06:00', '2020-01-01', '50%', '32.07°C'),
-(3, 2, '12:00', '2020-01-02', '50%', '29.05°C'),
-(3, 3, '18:00', '2020-01-03', '60%', '30.07°C');
+(1, 2, '12:00', '2020-12-03', '30%', '23.00°C'),
+(1, 3, '18:00', '2020-12-04', '25%', '23.00°C'),
+(2, 1, '06:00', '2020-05-02', '30%', '24.00°C'),
+(2, 2, '12:00', '2020-05-03', '50%', '21.00°C'),
+(2, 3, '18:00', '2020-05-04', '45%', '30.00°C'),
+(3, 1, '06:00', '2020-01-01', '50%', '32.00°C'),
+(3, 2, '12:00', '2020-01-02', '50%', '29.00°C'),
+(3, 3, '18:00', '2020-01-03', '60%', '30.00°C');
 
 
 alter table cliente add foreign key(fkParque) references parque(idParque);
@@ -77,7 +79,7 @@ select * from cliente,parque where fkParque = idParque; -- Mostra dados dos clie
 select * from cliente,parque where fkParque = idParque order by idCliente;-- Mostra dados dos clientes e seus respectivos parques ordenados pelo idCliente
 select * from parque,sensor where fkParque = idParque; -- Mostra os dados dos parques junto aos dados da tabela sensor
 select * from sensor,eventos where fkSensor = idSensor;-- Mostra os dados dos Sensores junto aos dados da tabela Eventos
-
+select * from cliente,eventos where idCliente = 1 and temperatura = '22.00°C'; -- mostra os dados do cliente 1 e os eventos que correspondem a sua temperatura ideal
 
 
 
