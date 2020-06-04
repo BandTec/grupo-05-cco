4ea1d3ce2e2ca128a9c5cbb2455f2ce13b3810b2
barbosa-victor <61800255+barbosa-victor@users.noreply.github.com>
2020-06-04 20:43:59 -0300

    Comeco da Implementacao do Node

---
+++ Site/app/database.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: '8889',
+    user: 'victor',
+    password: 'noobgamer200',
+    database: 'sensor'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Conectado com sucesso!')
+});
+
+
+
+module.exports = connection;
