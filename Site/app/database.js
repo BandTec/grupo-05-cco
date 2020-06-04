90b28efac58207549682f0a3fa033eba6a628b55
barbosa-victor <61800255+barbosa-victor@users.noreply.github.com>
2020-06-04 20:45:22 -0300

    Correcao

--- Site/app/database.js
+++ Site/app/database.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     port: '8889',
     user: 'victor',
-    password: 'noobgamer200',
+    password: 'coloquesenhaaqui',
     database: 'sensor'
 });
 
