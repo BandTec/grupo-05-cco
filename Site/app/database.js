c313f472736a1a5fb80cf1e9be6d2cc1ed60278b
kalil-bego <kalil.rocha@bandtec.com.br>
2020-06-08 20:41:55 -0300

    atualização que o Yoshi pediu

--- Site/app/database.js
+++ Site/app/database.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     port: '8889',
-    user: 'victor',
-    password: 'coloquesenhaaqui',
+    user: 'root',
+    password: 'Skrillex23',
     database: 'sensor'
 });
 
---
+++ projeto-aquisicao-local/projeto-node-local/banco.js
@@ -0,0 +1,33 @@
+const configuracoes = {
+    banco: {
+        server: "humildificadores.database.windows.net",
+        user: "adminlocal",
+        password: "Safadinhos123@",
+        database: "bdHumildificadores",
+        options: {
+            encrypt: true
+        },
+        pool: {
+            max: 4,
+            min: 1,
+            idleTimeoutMillis: 30000,
+            connectionTimeout: 5000
+        }
+    }
+}
+
+var sql = require('mssql');
+sql.on('error', err => {
+    console.error(`Erro de Conexão: ${err}`);
+});
+
+
+function conectar() {
+    sql.close();
+    return sql.connect(configuracoes.banco)
+}
+
+module.exports = {
+    conectar: conectar,
+    sql: sql
+}
