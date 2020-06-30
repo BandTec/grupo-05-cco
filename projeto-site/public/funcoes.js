4379cf85112e069259a46eada18d7974493fa04e
kalil-bego <kalil.rocha@bandtec.com.br>
2020-06-30 15:36:33 -0300

    Aba de patrocinio inserida

--- projeto-site/public/funcoes.js
+++ projeto-site/public/funcoes.js
@@ -13,9 +13,7 @@ function verificar_autenticacao() {
         loginDashboard.style.display = 'inline';
         cadastroDashboard.style.display = 'inline';
     } else {
-        loginDashboard.style.display = 'none';
-        cadastroDashboard.style.display = 'none';
-        lista.innerHTML = '<li><a href="dashboard.html">Dashboard</a></li> <li class="barra">|</li> <li><a href="#" id="b_usuario"></a></li> <li ><a href="#" onclick="logoff()">Sair</a></li>';
+        lista.innerHTML = '<li><a href="index.html">Início</a></li> <li><a href="dashboard.html">Dashboard</a></li> <li class="barra">|</li> <li><a href="#" id="b_usuario"></a></li> <li ><a href="#" onclick="logoff()">Sair</a></li>';
         b_usuario.innerHTML = nome_usuario;
         esconderStart.style.display = 'none';
         validar_sessao();
--- projeto-site/public/site/assets/js/validarCadastro.js
+++
@@ -1,4 +0,0 @@
-function validarCadastro(){
-    alert(`Bem-vindo ${nome.value}, seu cadastro foi realizado com sucesso!`);
-    window.location = "dashboard.html";
-}
