2b8e1af65f3c2ec3422952ffc8f2fd8dcfd8e13c
jusebandtec <jose.silva@bandtec.com.br>
2020-11-06 14:14:50 -0300

    Adicionando script da API Pythohms

---
+++ API's/API's SQL/MySQL Server/consultaptythohms.py
@@ -0,0 +1,44 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="bandtec",
+  database="projetoapi"
+)
+
+# print(mydb)
+global opcao_desejada
+global a
+
+while True:
+    opcao_desejada = input("Você deseja monitorar algo? [S/N]:\n")
+
+    if opcao_desejada.upper() == 'N':
+        break
+
+    meucursor = mydb.cursor()
+    select = meucursor.execute("SELECT column_name FROM information_schema.columns WHERE  table_name = 'pytohms' AND table_schema = 'projetoapi'")
+    meuresultado = meucursor.fetchall()
+    for x in meuresultado:
+        print("[{}]".format(x[0]))
+    
+    a = input("O que você deseja monitorar?:\n")
+
+    b = input("Quantos itens você deseja trazer?:\n")
+    print("\n ----------- Trazendo dados -------------\n")
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("select {} from pytohms limit {}".format(a,b))
+
+    myresult = mycursor.fetchall()
+
+    contador = 1
+    for y in myresult:
+        print(contador,"-", y[0])
+        contador+= 1
+
+
+
+    
