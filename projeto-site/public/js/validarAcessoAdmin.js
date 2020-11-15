87b8b8d5a4d2bc15709da926c1e92409c43d8d1d
jusebandtec <jose.silva@bandtec.com.br>
2020-11-15 16:42:36 -0300

    criação da página de login do administrador

--- projeto-site/public/js/validarAcessoAdmin.js
+++ projeto-site/public/js/validarAcessoAdmin.js
@@ -1,10 +1,10 @@
-// function validarAcesso() {
-//     var emailAcesso = usuario.value;
-//     var senhaAcesso = senha.value;
-//     if (emailAcesso == "admin@admin.com" && senhaAcesso == "admin") {
-//         alert("Você vai ser redirecionado...");
-//         window.location = "dashboard.html";
-//     } else {
-//         alert("Usuário ou Senha Inválidos");
-//     }
-// }
+function validarAcessoAdmin() {
+    var emailAcesso = usuarioAdmin.value;
+    var senhaAcesso = senhaAdmin.value;
+    if (emailAcesso == "admin@admin.com" && senhaAcesso == "admin") {
+        alert("Você vai ser redirecionado...");
+        window.location = "dashboard.html";
+    } else {
+        alert("Usuário ou Senha Inválidos");
+    }
+}
