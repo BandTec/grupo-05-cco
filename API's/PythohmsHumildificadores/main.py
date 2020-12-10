e2745d7d61c14044a0b664fa2b363649ac7672f0
kaio-baleeiro <kaio.jesus@bandtec.com.br>
2020-12-10 07:50:44 -0300

    Organização dos arquivos na API

--- API's/PythohmsHumildificadores/main.py
+++ API's/PythohmsHumildificadores/main.py
@@ -1,27 +1,7 @@
 from pythohms import CrawlerOpenHardwareMonitor
 from conexaobd import Mysql
-from maquinas import Maquina
 from automatizacao import Automatizando
 import time
 
-
-
-
-
 automatizacao = Automatizando()
 automatizacao.AutomatizandoScript()
-
-
-# maquina = Maquina(1,'kaiob', 1)
-# print(crawler.info['Desktop'])
-# x =  mysql.select('select * from maquinas where idMaquina= {}'.format(z))
-# while True:
-#     cont = 1
-#     print("TravouAqui: cont = 1")
-#     for config in configuracoes:
-#         print('Gerando {}º Dado: '.format(cont))
-#         dado = config.gerarDado(mysql)
-#         dado.inseriRegistro(mysql)
-#         print('\n')
-#         cont+=1
-#     time.sleep(2)
